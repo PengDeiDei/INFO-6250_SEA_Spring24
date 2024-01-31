e5de8c2259e91febba0b0fa046bbd449faee6b20
Haonan Peng <peng.haon@northeastern.edu>
2024-01-31 15:30:30 -0800

    Update work 02 and 03

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -3,17 +3,26 @@
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
 /*
- * since we only care about the common letters between two words,
- * it is better to use Set to remove duplicated letters in each word
- * before comparing
+ * Revise the function using Array rather than Set or Map following the 
+ * restriction. 
  */
 function compare( word, guess ) {  // DO NOT MODIFY
 /* YOU MAY MODIFY THE LINES BELOW */
-  const wordSet = new Set(word.toLowerCase());
-  const guessSet = new Set(guess.toLowerCase());
+  const wordArray = word.toLowerCase().split('');
+  const guessArray = guess.toLowerCase().split('');
 
-  // creat a new set that only contains common letters in both word and guess
-  const commonLettersSet = new Set([...wordSet].filter(letter => guessSet.has(letter)));
+  let count = 0;
 
-  return commonLettersSet.size; // this line is wrong
+  /* for each loop to go over each element in word array to check if the
+   * current element appears in the guess array; If found, remove the 
+   * first found element in the guess array and increase count.
+   */
+  wordArray.forEach(letter => {
+    if(guessArray.includes(letter)){
+      count++;
+      guessArray.splice(guessArray.indexOf(letter),1);
+    }
+  });
+
+  return count;
 }
